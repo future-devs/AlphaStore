d1097eaeb96ad341eaf8064f3990f7c3e83e92ee
NastyDaemon0614 <chitrankmishra010@gmail.com>
2021-01-07 20:19:19 +0530

    Initial Template

---
+++ static/Website/loader/loader.js
@@ -0,0 +1,8 @@
+// Toggling the loader funtion.
+function showLoader() {
+	document.getElementById('loaderDiv').style.display = 'block';
+}
+
+function hideLoader() {
+	document.getElementById('loaderDiv').style.display = 'none';
+}
