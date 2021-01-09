8b5fa471a8de8ca0c0af2b8191db3e773a2e5672
Anubhav Sinha <anubhavsinha98@gmail.com>
2021-01-09 13:22:19 +0530

    Added warehouse backend code

--- Warehouse/urls.py
+++ Warehouse/urls.py
@@ -2,4 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('warehouse/', views.warehouseHome, name='warehouseHome'),
+    path('warehouse/test', views.warehouseTest, name='warehouseTest'),
+    path("warehouse/get-all-products/",
+         views.getAllProducts, name="getAllProducts"),
+    path("warehouse/get-product/<int:pid>/",
+         views.getProductData, name="getProductData"),
+    path("warehouse/get-new-pid/", views.getNewPid, name="getNewPid"),
+    path("warehouse/add-product/", views.addProduct, name="addProduct"),
+    path("warehouse/delete-product/", views.deleteProduct, name="deleteProduct"),
+    path("warehouse/save-details/", views.saveDetails, name="saveDetails"),
+    path("warehouse/get-details/", views.getDetails, name="getDetails"),
 ]
--- Warehouse/views.py
+++ Warehouse/views.py
@@ -2,3 +2,43 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
+import pymongo
+import json
+
+connection_url = "mongodb+srv://admin:chitrank0614@basecluster.syalx.mongodb.net/Website?retryWrites=true&w=majority"
+
+client = pymongo.MongoClient(connection_url)
+ApiDatabase = client.get_database('Website')
+
+Products = ApiDatabase.Products
+SiteData = ApiDatabase.SiteData
+
+
+def getAllProducts(request):
+    query = Products.find()
+    output = []
+    for x in query:
+        del x['_id']
+        output.append(x)
+    return JsonResponse({'result': output})
+
+
+def getProductData(request, pid):
+    queryObj = {'ProductID': pid}
+    query = Products.find_one(queryObj)
+    del query['_id']
+    return JsonResponse({'result': query})
+
+
+def getNewPid(request):
+    pid = SiteData.find_one({'Data': 'NextProductID'})
+    pid = 'P'+str(pid['NextProductID'])
+    return JsonResponse({'result': pid})
+
+
+def increasePid():
+    pid = SiteData.find_one({'Data': 'NextProductID'})
+    pid['NextProductID'] += 1
+    query = SiteData.update_one({'Data': 'NextProductID'}, {
+                                '$set': pid}, upsert=True)
